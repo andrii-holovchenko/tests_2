8e1fb1b83e21dc5a60d9fcdfc2f3fd4d70f1c6b8
Andrii Holovchenko <golovchenko_a@ukr.net>
2019-02-20 21:13:51 +0000

    Multiple improvements in prog1.py

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # tests_2
+
+# Prog1.py 
+Usage:
+
+Create 3 files with size of 10 MB and place them to / mount point:
+./prog1.py -n 3 -s 10 -m 1 -p /
+
+List local mount points with free space more than 1000 MB:
+./prog1.py -l -m 100
--- prog1.py
+++ prog1.py
@@ -1,28 +1,66 @@
+#!/usr/bin/python
+
+import argparse
 import psutil
 import subprocess
 import re
 import time
+import multiprocessing
 
-file_num = 3
-file_size = 4096
-#free_space = 
+parser = argparse.ArgumentParser()
+parser.add_argument("-n", "--number", type=int, help="Number of files to create")
+parser.add_argument("-s", "--size", type=int, help="Size of file to create in MB")
+parser.add_argument("-m", dest='min_free', type=int, help="Minimum disk free space in MB")
+parser.add_argument("-l", dest="list",  action='store_true', help="List available mount points. Should be used with \'-m\' option")
+parser.add_argument("-p", dest='mount_point', type=str, help="Mount point to store files")
 
+args = parser.parse_args()
+
+file_num = args.number
+file_size = args.size
+min_free = args.min_free 
+mount_point = args.mount_point
 
 # Detect local mounted disk
-local_disk_mounts = []
-partitions = psutil.disk_partitions(all=False)
-pattern = re.compile(".*/dev/[h,s]d[a-z].*")
-for p in partitions:
-   if pattern.match(str(p)) and psutil.disk_usage(p.mountpoint).free > 1024:
-        local_disk_mounts.append(p.mountpoint)
+def local_disk_mounts(min_free):
+    local_disk_mounts = []
+    partitions = psutil.disk_partitions(all=False)
+    pattern = re.compile(".*/dev/(mapper|[h,s]d[a-z]).*")
+    for p in partitions:
+       # psutil.disk_usage returns values in bytes
+       if pattern.match(str(p)) and psutil.disk_usage(p.mountpoint).free > (min_free * 1024 * 1024):
+            local_disk_mounts.append(p.mountpoint)
+    return local_disk_mounts
 
 def run_dd(num, size, mount):
-    for n in range(num):
-        subprocess.check_output(['dd', 'if=/dev/zero', 'of=%sfile%s.txt' % (mount, n), 'count=1', 'bs=%s' % size ], stderr=subprocess.STDOUT)
+    try:
+        subprocess.check_output(['dd', 'if=/dev/zero', 'of=%sfile%s.txt' % (mount, num), 'status=none', 'count=1', 'bs=%s' % (size * 1024 * 1024)])
+        print('File \'%sfile%s.txt\' created succefully! Size: %s MB' % (mount, num, size))
+    except subprocess.CalledProcessError as e:
+        print e.output
 
+def list_disks():
+    print "Available mounted disks:"
+    print '\n'.join([i for i in local_disk_mounts(min_free)])
 
-start = time.time()
-run_dd(file_num, file_size, local_disk_mounts[0])
-end = time.time()
+if __name__ == '__main__':
+    start = time.time()
+    if args.list and min_free:
+        list_disks()
+        exit()
+    elif args.list and not min_free:
+        print "Option \'-l\' cant be use\'d without \'-m\' option!"
+        exit()
 
-print("Time used: " + str(end - start))
+    if args.number and args.size and args.mount_point:
+    # Check if specified mount point is available
+        if args.mount_point in local_disk_mounts(min_free):
+            processes = []
+            for i in range(file_num):
+                p = multiprocessing.Process(target=run_dd, args=(i, file_size, mount_point,))
+                processes.append(p)
+                p.start()
+            for process in processes:
+                process.join()
+    print('Time used {} seconds'.format(time.time() - start))
+
